7a3d7e0246d0726b6abfb84083ddc409b4d87892
dnaryshkin <dmitriynaryshkin@yandex.ru>
2025-07-04 22:45:32 +0300

    Добавлены словарь эмодзи, которое выводится в сообщении пользователю в зависимости от погоды в запрашиваемом городе. Реализона работа эмодзи в зависимости от погоды. Убраны лишние данные файла .env, которые не требуются на данной стадии проекта.

--- wether_bot.py
+++ wether_bot.py
@@ -10,9 +10,7 @@ from message import HI_MESSAGE, WEATHER_EMOJIS
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 API_TOKEN = os.getenv('API_TOKEN')
-ENDPOINT = os.getenv('ENDPOINT')
 TELEGRAM_NAME = os.getenv('TELEGRAM_NAME')
 
 bot = telebot.TeleBot(token=TELEGRAM_TOKEN)
@@ -22,8 +20,6 @@ def check_env():
     secret_token = {
         'TELEGRAM_TOKEN': TELEGRAM_TOKEN,
         'API_TOKEN': API_TOKEN,
-        'ENDPOINT': ENDPOINT,
-        'TELEGRAM_CHAT_ID': TELEGRAM_CHAT_ID,
         'TELEGRAM_NAME': TELEGRAM_NAME,
     }
 
@@ -43,6 +39,10 @@ def check_env():
             return True
 
 
+def check_availible_api():
+    """Функция проверки доступности API."""
+    pass
+
 @bot.message_handler(commands=['start'])
 def say_hello(message):
     """Функция отправки приветственного сообщения."""
@@ -50,18 +50,21 @@ def say_hello(message):
     chat_id = chat.id
     bot.send_message(chat_id=chat_id, text=HI_MESSAGE)
 
-
 @bot.message_handler(content_types=['text'])
 def find_city(message):
-    """Функция определения города по которому необходимо сделать прогноз."""
+    """
+    Функция получения от пользователя текста названия города по которому
+    необходимо предоставить прогноз.
+    """
     text_message = message.text
     chat = message.chat
     chat_id = chat.id
     find_geo(message, text_message)
 
 def find_geo(message, text_message):
-    """Функция получения долготы и широты города."""
-    URL = f'http://api.openweathermap.org/geo/1.0/direct?q={text_message}&appid={API_TOKEN}'
+    """Функция получения долготы и широты города из текста пользователя."""
+    URL = (f'http://api.openweathermap.org/geo/1.0/direct?q={text_message}'
+           f'&appid={API_TOKEN}')
     response = requests.get(URL).json()
     lat = response[0]['lat']
     lon = response[0]['lon']
@@ -70,21 +73,24 @@ def find_geo(message, text_message):
 
 def find_weather(message, lat, lon):
     """Функция получения прогноза погоды по долготе и широте."""
-    URL = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_TOKEN}&units=metric'
+    URL = (f'https://api.openweathermap.org/data/2.5/weather?lat={lat}'
+           f'&lon={lon}&lang=ru&appid={API_TOKEN}&units=metric')
     weather_data = requests.get(URL).json()
     city = weather_data['name']
     country = weather_data['sys']['country']
     temperature = weather_data['main']['temp']
     feels_like = weather_data['main']['feels_like']
-    weather_desc = weather_data['weather'][0]['description']
     weather_icon = weather_data['weather'][0]['icon']
-    wind_speed = weather_data['wind']['speed']
+    weather_desc = weather_data['weather'][0]['description']
     humidity = weather_data['main']['humidity']
     pressure = weather_data['main']['pressure']
+    wind_speed = weather_data['wind']['speed']
+    deg_wind_speed = weather_data['wind']['deg']
+
     weather_report = (
-        f"Погода в {city}, {country}:\n"
-        f"🌡 Температура: {temperature}°C (ощущается как {feels_like}°C)\n"
-        f"☁️ Состояние: {weather_desc.capitalize()}\n"
+        f'Погода в {city}, страна({country}):\n'
+        f'🌡️Температура: {temperature}°C (ощущается как {feels_like}°C)\n'
+        f' {WEATHER_EMOJIS[weather_icon]}Состояние: {weather_desc.capitalize()}\n'
         f"💧 Влажность: {humidity}%\n"
         f"🎚 Давление: {pressure} гПа\n"
         f"🌬 Ветер: {wind_speed} м/с"
